dcc247d4ff126f3bc3737ab12b5ba474e4f6bbbf
unknown <you@example.comtanzilatania31@gmail.com>
2023-05-15 23:53:18 +0600

    Done

---
+++ assignment_3.dart
@@ -0,0 +1,70 @@
+class Car {
+  late String brand;
+  late String model;
+  late int year;
+  late double milesDriven;
+  static int numberOfCars = 0;
+
+  Car(this.brand, this.model, this.year) {
+    milesDriven = 0;
+    numberOfCars++;
+  }
+
+  void drive(double miles) {
+    milesDriven += miles;
+  }
+
+  double getMilesDriven() {
+    return milesDriven;
+  }
+
+  String getBrand() {
+    return brand;
+  }
+
+  String getModel() {
+    return model;
+  }
+
+  int getYear() {
+    return year;
+  }
+
+  int getAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
+
+void main() {
+  Car car1 = Car("BMW", "BMW X5", 2018);
+  Car car2 = Car("Toyota", "Corolla", 2018);
+  Car car3 = Car("Honda", "Accord", 2020);
+
+  car1.drive(1000);
+  car2.drive(20000);
+  car3.drive(30000);
+
+  print("Car 1:");
+  print("Brand: ${car1.getBrand()}");
+  print("Model: ${car1.getModel()}");
+  print("Year: ${car1.getYear()}");
+  print("Miles driven: ${car1.getMilesDriven()}");
+  print("Age: ${car1.getAge()}");
+
+  print("Car 2:");
+  print("Brand: ${car2.getBrand()}");
+  print("Model: ${car2.getModel()}");
+  print("Year: ${car2.getYear()}");
+  print("Miles driven: ${car2.getMilesDriven()}");
+  print("Age: ${car2.getAge()}");
+
+  print("Car 3:");
+  print("Brand: ${car3.getBrand()}");
+  print("Model: ${car3.getModel()}");
+  print("Year: ${car3.getYear()}");
+  print("Miles driven: ${car3.getMilesDriven()}");
+  print("Age: ${car3.getAge()}");
+
+  print("Total number of cars created: ${Car.numberOfCars}");
+}
